5631fae741f78ba2d5017bfc83c9d2c9ea966a60
Parasuram <Parasuram.Tanguturu@ruckuswireless.com>
2022-11-27 08:59:07 +0530

    Initial commit

---
+++ src/main/java/com/parasuram/server/BankService.java
@@ -0,0 +1,20 @@
+package com.parasuram.server;
+
+
+import com.parasuram.models.Balance;
+import com.parasuram.models.BalanceCheckRequest;
+import io.grpc.stub.StreamObserver;
+
+public class BankService extends com.parasuram.models.BankServiceGrpc.BankServiceImplBase {
+    @Override
+    public void getBalance(BalanceCheckRequest request, StreamObserver<Balance> responseObserver) {
+        final int accountNumber = request.getAccountNumber();
+        final Balance balance = Balance.newBuilder()
+                .setAmount(accountNumber * 10)
+                .build();
+
+        responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+
+    }
+}
