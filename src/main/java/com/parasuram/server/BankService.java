59693be95d6f853c42f14af1fecb77aad3ffb149
Parasuram <Parasuram.Tanguturu@ruckuswireless.com>
2022-12-01 15:19:03 +0530

    BankService - server side streaming

--- src/main/java/com/parasuram/server/BankService.java
+++ src/main/java/com/parasuram/server/BankService.java
@@ -3,9 +3,17 @@ package com.parasuram.server;
 
 import com.parasuram.models.Balance;
 import com.parasuram.models.BalanceCheckRequest;
+import com.parasuram.models.Money;
+import com.parasuram.models.WithDrawRequest;
 import io.grpc.stub.StreamObserver;
 
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+
 public class BankService extends com.parasuram.models.BankServiceGrpc.BankServiceImplBase {
+
+    protected final Logger logger = LogManager.getLogManager().getLogger("bankService");
     @Override
     public void getBalance(BalanceCheckRequest request, StreamObserver<Balance> responseObserver) {
         final int accountNumber = request.getAccountNumber();
@@ -14,6 +22,29 @@ public class BankService extends com.parasuram.models.BankServiceGrpc.BankServic
                 .build();
 
         responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+
+    }
+
+    @Override
+    public void withDraw(WithDrawRequest request, StreamObserver<Money> responseObserver) {
+        int account =request.getAccountNumber();
+        int amount=request.getAmount(); //10,20...100
+        int balance=AccountDataBase.getBalance(account);
+        System.out.println("account: "+account+" amount: "+amount +" balance: "+balance);
+        for (int i=0;i<(amount/10);i++){
+            Money money=com.parasuram.models.Money.newBuilder()
+                                    .setValue(10).build();
+            System.out.println("money is:"+money);
+            responseObserver.onNext(money);
+            AccountDataBase.deductBalance(account,10);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("account: "+account+" amount: "+account +" balance: "+AccountDataBase.getBalance(account));
         responseObserver.onCompleted();
 
     }
--- src/test/java/com/parasuram/client/BankClientTest.java
+++ src/test/java/com/parasuram/client/BankClientTest.java
@@ -1,18 +1,21 @@
 package com.parasuram.client;
 
 import com.parasuram.models.BankServiceGrpc;
+import com.parasuram.models.Money;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.Iterator;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BankClientTest {
      BankServiceGrpc.BankServiceBlockingStub bankServiceBlockingStub;
     @BeforeAll
     public void setup(){
-        ManagedChannel managedChannel=ManagedChannelBuilder.forAddress("localhost",6565)
+        ManagedChannel managedChannel=ManagedChannelBuilder.forAddress("localhost",6567)
                 .usePlaintext()
                 .build();
         this.bankServiceBlockingStub= BankServiceGrpc.newBlockingStub(managedChannel);
@@ -26,5 +29,17 @@ public class BankClientTest {
                 .build();
         com.parasuram.models.Balance balance=this.bankServiceBlockingStub.getBalance(balanceCheckRequest);
         System.out.println("balance is: "+balance);
+    }
+    @Test
+    public  void withDrawTest(){
+        com.parasuram.models.WithDrawRequest withDrawRequest= com.parasuram.models.WithDrawRequest
+                .newBuilder()
+                .setAmount(40)
+                .setAccountNumber(7)
+                .build();
+        Iterator<Money> money=this.bankServiceBlockingStub.withDraw(withDrawRequest);
+
+       money.forEachRemaining(System.out::println);
+
     }
 }
